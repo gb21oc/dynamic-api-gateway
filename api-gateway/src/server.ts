76bad3c6cc89d59474a2dac802d3ac1336dbe2a9
gabriel <gabrieljoseDev77@outlook.com>
2023-10-16 14:08:00 -0300

    refactor(server) apenas melhorando a legibilidade

--- api-gateway/src/server.ts
+++ api-gateway/src/server.ts
@@ -4,21 +4,34 @@ import { resolve } from "path"
 const path = resolve(__dirname, `../.${process.env.NODE_ENV}.env`)
 dotenv.config({path: path})
 
+import helmet from "helmet"
 import express from "express"
 import { AppDataSource } from "./config/typeORM.config"
+import { ServiceShared } from "./common/shared/service.shared"
+import { configDynamicRouter } from "./routes/config-routes-dynamic/config-routes-dynamic.controller"
+
 
 (async () => {
     const port = 8080
     const app = express()
+    const serviceShared = new ServiceShared()
+
+    // * Config express
+    app.use(helmet())
+    app.use(express.json())
+    app.disable('x-powered-by');
+    
 
     // * Database
     const dataSource = await AppDataSource.initialize()
-    console.log(`[+] Database is initialize: ${dataSource.isInitialized}`)
+    serviceShared.consoleLogColor(`\x1b[33m[Database]\x1b[0m \x1b[32mDatabase is initialize: ${dataSource.isInitialized}\x1b[0m`)
 
-    app.use(express.json())
-    //app.use("/dynamic", require("../src/routes/gateway/gateway.route"))
-    //app.use("/dynamic-config", configGateway)
+    // * Routes
+    app.use("/dynamic", require("../src/routes/gateway/gateway.controller"))
+    app.use("/dynamic-config", configDynamicRouter)
+
+    // * Server
     app.listen(port, () => {
-        console.log(`[+] App listering on port ${port}`)
+        serviceShared.consoleLogColor(`\x1b[33m[ExpressApplication]\x1b[0m \x1b[32mExpress Application successfully stated in url http://localhost:${port}/\x1b[0m`)
     })
 })()
