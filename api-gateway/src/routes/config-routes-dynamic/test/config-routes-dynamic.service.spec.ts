7fc3bd68ba79aa97d3483297c1f4e1e048d2eb7e
gabriel <gabrieljoseDev77@outlook.com>
2023-10-17 14:55:28 -0300

    test(service) criado caso de teste para deletar e atualizar rota

--- api-gateway/src/routes/config-routes-dynamic/test/config-routes-dynamic.service.spec.ts
+++ api-gateway/src/routes/config-routes-dynamic/test/config-routes-dynamic.service.spec.ts
@@ -14,15 +14,15 @@ import { configRoutesDynamicServiceMOCK } from "../../../../test/mock/config-rou
 import { DataSource } from "typeorm";
 import { AppDataSource } from "../../../config/typeORM.config";
 import { IConfigRoutesDynamic } from "../../../common/interface/routes/configRoutesDynamic.interface";
+import { RouteEntity } from "../../../entity/route.entity";
 
 describe("Config Routes Dynamic Service", () => {
-    //let dataSource: DataSource;
     const configRoutesDynamicService = new ConfigRoutesDynamicService()
-
-    /* beforeAll(async () => {
-        dataSource = await AppDataSource.initialize()
-        console.log(`[+] Database is initialize: ${dataSource.isInitialized}`) 
-    }) */
+    const repo = AppDataSource.getRepository(RouteEntity)
+    const sypOnFindAll = jest.spyOn(repo, "find").mockReturnValue(Promise.resolve(repositoryMOCK.mockSuccessFindAll))
+    const sypOnUpdate = jest.spyOn(repo, "update").mockReturnValue(Promise.resolve(repositoryMOCK.mockSuccessUpdate))
+    const sypOnInsert = jest.spyOn(repo, "save").mockReturnValue(Promise.resolve(repositoryMOCK.mockSuccessInsertInDatabase))
+    const sypOnDelete = jest.spyOn(repo, "delete").mockReturnValue(Promise.resolve(repositoryMOCK.mockSuccessDeleteItem))
 
     describe("validateBody", () => {
         const body = new RouteEntityDTO({
@@ -58,11 +58,8 @@ describe("Config Routes Dynamic Service", () => {
 
     describe("getAll", () => {
         it("should return all data in database", async () => {
-            const sypOnFindAll = jest.spyOn(RouteRepository.prototype, "findAll").mockReturnValue(Promise.resolve(repositoryMOCK.mockSuccessFindAll))
             const data = await configRoutesDynamicService.getAll()
             expect(data.statusCode).toBe(200)
-            sypOnFindAll.mockClear()
-            sypOnFindAll.mockRestore()
         })
 
         it("should return empty, status-code is not-found", async () => {
@@ -75,12 +72,9 @@ describe("Config Routes Dynamic Service", () => {
     })
 
     describe("getById", () => {
-        it("should return the object corresponding to the ID", async () => {
-            const sypOnFindById = jest.spyOn(RouteRepository.prototype, "findById").mockReturnValue(Promise.resolve(repositoryMOCK.mockSuccessFindAll))            
+        it("should return the object corresponding to the ID", async () => {           
             const data = await configRoutesDynamicService.getById("1")
             expect(data.statusCode).toBe(200)
-            sypOnFindById.mockClear()
-            sypOnFindById.mockRestore()
         })
 
         it("should return not-found", async () => {
@@ -122,14 +116,8 @@ describe("Config Routes Dynamic Service", () => {
         })
 
         it("should return route already created", async () => {
-            const sypOnInsert = jest.spyOn(RouteRepository.prototype, "insert").mockReturnValue(Promise.resolve(repositoryMOCK.mockSuccessInsertInDatabase))
-            const sypOnLike = jest.spyOn(RouteRepository.prototype, "findByPathAndMethodLikeMicroService").mockReturnValue(Promise.resolve(repositoryMOCK.mockSuccessInExecutingQueryLike))
             const data = await configRoutesDynamicService.createRoute(body)
             expect(data).toMatchObject(configRoutesDynamicServiceMOCK.mockRouteAlreadyCreated)
-            sypOnInsert.mockClear()
-            sypOnInsert.mockRestore()
-            sypOnLike.mockClear()
-            sypOnLike.mockRestore()
         })
 
         it("should return bad-request becouse field 'isBody' is '1' and 'method' is 'GET' it is only possible to use body when it is 'POST, PUT, PATCH'", async () => {
@@ -143,12 +131,30 @@ describe("Config Routes Dynamic Service", () => {
         })
     })
 
-    /* afterAll(async () => {
-        await dataSource.destroy()
-    }) */
+    describe("deleteRoute", () => {
+        it("should return success in delete item", async () => {
+            const data = await configRoutesDynamicService.deleteRoute("8")
+            expect(data).toMatchObject(configRoutesDynamicServiceMOCK.mockSuccessDeleteRoute)
+        })
+    })
+
+    describe("updateRoute", () => {
+        it("", async () => {
+            const data = await configRoutesDynamicService.updateRoute("8", {
+                path: "teste"
+            })
+            expect(data).toMatchObject(configRoutesDynamicServiceMOCK.mockSuccessUpdateItem)
+        })
+    })
+
+    afterAll(async () => {
+        sypOnFindAll.mockClear()
+        sypOnFindAll.mockRestore()
+        sypOnUpdate.mockClear()
+        sypOnUpdate.mockRestore()
+        sypOnInsert.mockClear()
+        sypOnInsert.mockRestore()
+        sypOnDelete.mockClear()
+        sypOnDelete.mockRestore()
+    })
 })
-
-/**
-
-
- */
